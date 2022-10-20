4e5f1cc4d97f68db14f422031cf0961113f9d80e
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2022-10-20 10:58:33 -0700

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -125,7 +125,7 @@ int print_hexa_upper(va_list types, char buffer[],
  * @precision: Precision specification
  * @size: Size specifier
  * @size: Size specification
- * Return: Number of chars printed
+ * Return: Number of chars printed, this is a team work
  */
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
