0d801f46e24c6cfcd5014a3657a286cb5bfa72ab
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2022-10-20 11:02:05 -0700

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -4,7 +4,7 @@
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @list: list of arguments, team work.
  *
  * Return: Precision.
  */
