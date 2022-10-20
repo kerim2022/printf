3ad8371a36df39bd79c314e1761c7c9102c8f8bc
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2022-10-20 11:02:47 -0700

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -5,7 +5,7 @@
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
- * Return: Precision.
+ * Return: Precision, team work
  */
 int get_size(const char *format, int *i)
 {
