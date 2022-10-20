094ca909eb40e413fed3d5b7736ad25fa5398897
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2022-10-20 10:56:19 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -6,6 +6,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
+ *team work
  */
 
 int _printf(const char *format, ...)
